98ebb2dba236e0f3caf888bf961cf2bb5d0236e1
kirisaki <735647137@qq.com>
2024-01-06 02:25:16 +0800

    更改前端页面布局

--- kirisaki-generator-web-frontend/config/defaultSettings.ts
+++ kirisaki-generator-web-frontend/config/defaultSettings.ts
@@ -9,19 +9,21 @@ const Settings: ProLayoutProps & {
 } = {
   navTheme: 'light',
   // 拂晓蓝
-  colorPrimary: '#1890ff',
+  colorPrimary: '#722ED1',
   layout: 'mix',
-  contentWidth: 'Fluid',
+  contentWidth: 'Fixed',
   fixedHeader: false,
   fixSiderbar: true,
   colorWeak: false,
   title: '鱼皮前端万用模板',
   pwa: true,
   iconfontUrl: '',
+  siderMenuType: "sub",
   token: {
     // 参见ts声明，demo 见文档，通过token 修改样式
     //https://procomponents.ant.design/components/layout#%E9%80%9A%E8%BF%87-token-%E4%BF%AE%E6%94%B9%E6%A0%B7%E5%BC%8F
   },
+
 };
 
 export default Settings;
