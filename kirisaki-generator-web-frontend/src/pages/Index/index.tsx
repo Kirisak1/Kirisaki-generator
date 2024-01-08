f3afa078444d7ad63905bcea8f6bb7b3580e3557
kirisaki <735647137@qq.com>
2024-01-08 17:09:49 +0800

    主页(搜索页) 完成

--- kirisaki-generator-web-frontend/src/pages/Index/index.tsx
+++ kirisaki-generator-web-frontend/src/pages/Index/index.tsx
@@ -1,6 +1,8 @@
 import { listGeneratorVoByPage } from '@/services/backend/generatorController';
-import { PageContainer } from '@ant-design/pro-components';
-import { message } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+import { PageContainer, ProFormSelect, ProFormText, QueryFilter } from '@ant-design/pro-components';
+import { Avatar, Card, Flex, Image, Input, List, message, Tabs, Tag, Typography } from 'antd';
+import moment from 'moment';
 import React, { useEffect, useState } from 'react';
 
 /**
@@ -12,31 +14,165 @@ const DEFAULT_PAGE: PageRequest = {
   sortField: 'createTime',
   sortOrder: 'descend',
 };
-
+/**
+ * 主页
+ * @constructor
+ */
 const IndexPage: React.FC = () => {
   //设置变量
-  const [loading, setLoading] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(true);
   const [dataList, setDataList] = useState<API.GeneratorVO[]>([]);
   const [total, setTotal] = useState<number>(0);
-  const [searchText, setSearchText] = useState<API.GeneratorQueryRequest>({
+  //搜索条件
+  const [searchParams, setSearchParams] = useState<API.GeneratorQueryRequest>({
     ...DEFAULT_PAGE,
   });
+  /**
+   * 搜索
+   */
   const doSearch = async () => {
     setLoading(true);
     try {
-      const res = await listGeneratorVoByPage(searchText);
+      const res = await listGeneratorVoByPage(searchParams);
       setDataList(res.data?.records ?? []);
       setTotal(Number(res.data?.total ?? 0));
-    } catch (errror: any) {
-      message.error('获取数据失败' + errror.message);
+    } catch (error: any) {
+      message.error('获取数据失败' + error.message);
     }
     setLoading(false);
   };
   useEffect(() => {
     doSearch();
-  }, [searchText]);
+  }, [searchParams]);
 
-  return <PageContainer></PageContainer>;
+  /**
+   * 标签列表
+   * @param tags 标签属性
+   */
+  const tagListView = (tags?: string[]) => {
+    if (!tags) {
+      return <></>;
+    }
+    return (
+      <div style={{ marginBottom: 8 }}>
+        {tags.map((tag) => (
+          <Tag key={tag}> {tag}</Tag>
+        ))}
+      </div>
+    );
+  };
+  return (
+    <PageContainer title={<></>}>
+      <Flex justify="center">
+        <Input.Search
+          style={{
+            width: '40vw',
+            minWidth: 320,
+          }}
+          placeholder="请输入搜索内容"
+          allowClear
+          enterButton="搜索"
+          size="large"
+          onChange={(e) => {
+            searchParams.searchText = e.target.value;
+          }}
+          onSearch={(value) => {
+            setSearchParams({
+              ...DEFAULT_PAGE,
+              searchText: value,
+            });
+          }}
+        />
+      </Flex>
+      <div style={{ marginBottom: 16 }} />
+      {/* tags栏 */}
+      <Tabs
+        size={'large'}
+        defaultActiveKey="newest"
+        items={[
+          {
+            key: 'newest',
+            label: '最新',
+          },
+          {
+            key: 'recommend',
+            label: '推荐',
+          },
+        ]}
+        onChange={() => {}}
+      />
+      {/*查询过滤器*/}
+      <QueryFilter
+        span={12}
+        labelWidth="auto"
+        labelAlign="left"
+        defaultCollapsed={false}
+        style={{ padding: '16px 0' }}
+        onFinish={async (values:API.GeneratorQueryRequest) => {
+          setSearchParams({
+            ...DEFAULT_PAGE,
+            ...values,
+            searchText: searchParams.searchText,
+          });
+        }}
+      >
+        <ProFormSelect name="tags" label="标签" mode={'tags'} />
+        <ProFormText name="name" label="名称" />
+        <ProFormText name="description" label="描述" />
+      </QueryFilter>
+      <div style={{ marginBottom: 24 }} />
+
+      <List<API.GeneratorVO>
+        rowKey="id"
+        loading={loading}
+        grid={{
+          gutter: 16,
+          xs: 1,
+          sm: 2,
+          md: 3,
+          lg: 3,
+          xl: 4,
+          xxl: 4,
+        }}
+        dataSource={dataList}
+        pagination={{
+          current: searchParams.current,
+          pageSize: searchParams.pageSize,
+          total: total,
+          onChange(current, pageSize) {
+            setSearchParams({
+              ...searchParams,
+              current,
+              pageSize,
+            });
+          },
+        }}
+        renderItem={(data) => (
+          <List.Item>
+            <Card hoverable cover={<Image alt={data.name} src={data.picture} />}>
+              <Card.Meta
+                title={<a>{data.name}</a>}
+                description={
+                  <Typography.Paragraph ellipsis={{ rows: 2 }} style={{ height: 44 }}>
+                    {data.description}
+                  </Typography.Paragraph>
+                }
+              />
+              {tagListView(data.tags)}
+              <Flex justify="space-between" align="center">
+                <Typography.Text type="secondary" style={{ fontSize: 12 }}>
+                  {moment(data.createTime).fromNow()}
+                </Typography.Text>
+                <div>
+                  <Avatar src={data.user?.userAvatar ?? <UserOutlined />} />
+                </div>
+              </Flex>
+            </Card>
+          </List.Item>
+        )}
+      />
+    </PageContainer>
+  );
 };
 
 export default IndexPage;
