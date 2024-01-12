fd41558234ef9dc23e5283d4895ee25bd9f38abc
kirisaki <735647137@qq.com>
2024-01-12 21:13:39 +0800

    下载生成器接口

--- kirisaki-generator-web-backend/src/main/java/com/yupi/web/controller/GeneratorController.java
+++ kirisaki-generator-web-backend/src/main/java/com/yupi/web/controller/GeneratorController.java
@@ -1,7 +1,11 @@
 package com.yupi.web.controller;
 
+import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.qcloud.cos.model.COSObject;
+import com.qcloud.cos.model.COSObjectInputStream;
+import com.qcloud.cos.utils.IOUtils;
 import com.yupi.web.annotation.AuthCheck;
 import com.yupi.web.common.BaseResponse;
 import com.yupi.web.common.DeleteRequest;
@@ -10,6 +14,7 @@ import com.yupi.web.common.ResultUtils;
 import com.yupi.web.constant.UserConstant;
 import com.yupi.web.exception.BusinessException;
 import com.yupi.web.exception.ThrowUtils;
+import com.yupi.web.manager.CosManager;
 import com.yupi.web.meta.Meta;
 import com.yupi.web.model.dto.Generator.GeneratorAddRequest;
 import com.yupi.web.model.dto.Generator.GeneratorEditRequest;
@@ -26,6 +31,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -44,6 +51,9 @@ public class GeneratorController {
 
     @Resource
     private UserService userService;
+
+    @Resource
+    private CosManager cosManager;
 
     // region 增删改查
 
@@ -267,4 +277,50 @@ public class GeneratorController {
         return ResultUtils.success(result);
     }
 
+    /**
+     * 根据 id 下载
+     *
+     * @param id       生成器id
+     * @param request
+     * @param response
+     * @return
+     */
+    @GetMapping("/download")
+    public void downloadGeneratorById(long id, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        if (id <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        //获取当前登录用户
+        User loginUser = userService.getLoginUser(request);
+        //获取需要下载的生成器
+        Generator generator = generatorService.getById(id);
+        if (generator == null) {
+            throw new BusinessException(ErrorCode.NOT_FOUND_ERROR);
+        }
+        String filePath = generator.getDistPath();
+        if (StrUtil.isBlank(filePath)) {
+            throw new BusinessException(ErrorCode.NOT_FOUND_ERROR,"产物包不存在");
+        }
+        log.info("用户{},下载了{}", loginUser, filePath);
+        //文件下载
+        COSObjectInputStream cosObjectInput = null;
+        try {
+            COSObject cosObject = cosManager.getObject(filePath);
+            cosObjectInput = cosObject.getObjectContent();
+            byte[] bytes = IOUtils.toByteArray(cosObjectInput);
+            response.setContentType("application/octet-stream,charSet=UTF-8");
+            response.setHeader("Content-Disposition", "attachment;filename=" + filePath);
+            response.getOutputStream().write(bytes);
+            //刷新流, 不刷新就下载不到
+            response.getOutputStream().flush();
+        } catch (Exception e) {
+            log.error("file download error, filepath = " + filePath, e);
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "下载失败");
+        } finally {
+            if (cosObjectInput != null) {
+                cosObjectInput.close();
+            }
+        }
+
+    }
 }
